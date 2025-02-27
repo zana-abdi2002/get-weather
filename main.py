3b5e52b7f68d0532bb22f7e651dbc6118fb0328a
Zana-Abdi <Zana.Abdi@hotmail.com>
2025-02-27 10:35:13 +0330

    feat(main.py, .gitignore) add argparse module to parse user input and get help
    Added argparse module to main.py to parse user input and provide help functionality.
    add four add_mutually_exclusive_group arguments so they can't be used together.
    
    argparse arguments:
    positional arguments:
        COMMAND              use one of these commands:
                                get: gets weather of specified city for example: get new york
    
        CITY                 insert the city you want to track
    
        options:
        -h, --help           show this help message and exit
        -n, --now            shows current weather
        -t, --today          shows today's weather forcast
        -x, --next-day       shows tomorrow's weather forcast
        -r, --recent RECENT  shows recent weather events for the given number of days
    
    Modified .gitignore to ignore .env file, which contains sensitive environment variables.
    
    TESTING: use these commands and print(*[f"{k}={v}" for k, v in vars(args).items()]):
    get sanandaj -t (OR --today)
    get sanandaj -n (OR --now)
    get sanandaj -x (OR --next-day)
    get sanandaj -r (OR --recent)
    
    get new york -t (TEST two words city name)
    get new york -tn (TEST conflicting optional arguments)
    get new york -t -n (TEST conflicting optional arguments)
    got new york -t (TEST wrong command argument)
    
    -h (TEST help)

--- main.py
+++ main.py
@@ -2,51 +2,102 @@
 # -*- coding: utf-8 -*-
 
 
-def fetch_api():
-    pass
-
-
-def get_city_weather(city_name):
-    pass
-
-
-def main():
+def cli():
     user_input = (
         input("\033[92m\033[1mgetWeather \033[0m").lower().strip().split(" ")
     )  # green and bold
+
+    try:
+        args = parser.parse_args(user_input)
+        print(*[f"{k}={v}" for k, v in vars(args).items()])
+    except SystemExit as ex:
+        print(ex)
+        return -1
+
+    if args.now:
+        pass
+    elif args.today:
+        pass
+    elif args.next_day:
+        pass
+    elif args.recent:
+        pass
+    else:
+        pass
+
+
+def main():
+    # initialize argparse to parse user commands and get help -------------------------------------------
+    # run -h or --help to see help
+    import argparse
+
+    global parser  # so it's usable inside cli function
+    parser = argparse.ArgumentParser(
+        description="Provides weather details for the specified city."
+    )
 
     parser.add_argument(
         "command",
         type=str,
         help=(
-            "use one of these commands\n   get: gets weather of specified city for example: get newyork\n"
+            "use one of these commands\n\n   get: gets weather of specified city for example: get new york\n"
         ),
         metavar="COMMAND",
         default="help",
-        nargs=("*"),
+        choices=["get"],
+        nargs=(1),
     )
 
-    args = parser.parse_args(user_input)
+    parser.add_argument(
+        "city_name",
+        type=str,
+        help="insert the city you want to track",
+        metavar="CITY",
+        nargs=("+"),
+    )
 
-    command = args.command
-    if command[0] == "get":
-        city_name = "".join(command[1:])
-        get_city_weather(city_name)
-    elif command[0] == "exit":
-        pass
-    else:
-        parser.print_help()
-        return 0
+    features = (
+        parser.add_mutually_exclusive_group()
+    )  # only one of the arguments can be used
+    features.add_argument(
+        "-n",
+        "--now",
+        action="store_true",
+        help="shows current weather",
+    )
+    features.add_argument(
+        "-t",
+        "--today",
+        action="store_true",
+        help="shows today's weather forcast",
+    )
+    features.add_argument(
+        "-x",
+        "--next-day",
+        action="store_true",
+        help="shows tomorrow's weather forcast",
+    )
+    features.add_argument(
+        "-r",
+        "--recent",
+        action="append",
+        type=int,
+        help="shows recent weather events for the given number of days",
+    )
+    # -----------------------------------------------------------------------------------------------
 
-    return 1
+    # * Import fetching API Modules and data ---------------------------------------------------------
+    # from dotenv import load_dotenv
+    # from os import environ
+
+    # load_dotenv()
+    # global api_key
+    # api_key = environ.get("WEATHER_API_KEY")
+    # * ----------------------------------------------------------------------------------------------
+
+    while True:
+        cli()
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(
-        description="Provides weather details for the specified city."
-    )
-
-    while True:
-        main()
+    main()
